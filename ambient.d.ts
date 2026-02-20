0ef61bf48d7796c3f4d2151ed38c5de5d025501f
Raphael Wirnsberger <rwirnsberger@anexia-it.com>
2026-02-20 18:08:00 +0100

    build: add GNOME 49 gir deps and update TS config
    Ensure the project builds against GNOME Shell 49 by moving gir packages to dependencies and adjusting tsconfig for the new target.

---
+++ ambient.d.ts
@@ -0,0 +1,5 @@
+import "@girs/gjs";
+import "@girs/gjs/dom";
+import "@girs/soup-3.0";
+import "@girs/gnome-shell/ambient";
+import "@girs/gnome-shell/extensions/global";
--- gnome-shell.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="./node_modules/@girs/gjs/dom.d.ts" />
-/// <reference path="./node_modules/@girs/gjs/gjs.d.ts" />
-/// <reference path="./node_modules/@girs/gnome-shell/dist/index-ambient.d.ts" />
-/// <reference path="./node_modules/@girs/gnome-shell/dist/extensions/global.d.ts" />
-/// <reference path="./node_modules/@girs/soup-3.0/soup-3.0-ambient.d.ts" />
